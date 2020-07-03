56d8658b0e81fa8b64415614e1512d19fedf8e4b
kangkk <365@kangkk.com>
2020-07-03 17:38:05 +0800

    增加跳转

--- view/common/Header.vue
+++ view/common/Header.vue
@@ -1,29 +1,32 @@
-
 <template>
-    <el-header id="header">
-        <div class="h-top l36 clearfix box">
-            <div class="pull-left h-phone c-green mr40">
-                <span></span>
-                免费咨询：400-100-1111
+    <div id="test">
+        <test></test>
+        <el-header id="header">
+            <div class="h-top l36 clearfix box">
+                <div class="pull-left h-phone c-green mr40">
+                    <span></span>
+                    免费咨询：400-100-1111
+                </div>
+                <div class="pull-left h-time c-9">
+                    <span></span>
+                    工作时间：9:00-18:00
+                </div>
+                <el-input placeholder="请输入内容" class="h-search" clearable v-model="inputSearch">
+                    <el-button slot="append" icon="el-icon-search" class="h-searchBT"></el-button>
+                </el-input>
             </div>
-            <div class="pull-left h-time c-9">
-                <span></span>
-                工作时间：9:00-18:00
+            <div class="clearfix box">
+
             </div>
-            <el-input placeholder="请输入内容" class="h-search" clearable v-model="inputSearch">
-                <el-button slot="append" icon="el-icon-search" class="h-searchBT"></el-button>
-            </el-input>
-        </div>
-        <div class="clearfix box">
 
-        </div>
+        </el-header>
 
-    </el-header>
+    </div>
 </template>
 
 <script>
+    import test from "./test";
     import axios from "axios";
-    import BS from "../../public/js/common/BinarySearch.js";
 
     export default {
         name: "ly-header",
@@ -37,7 +40,7 @@
         mounted() {
             console.log('here is header')
         },
-
+        components: {test},
         methods: {},
     }
 </script>
---
+++ view/common/test.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="testconnect" style="background: black;width: 100%;height: 50px; display: block">
+        <div style="margin:0 20px;display:inline;">
+            <a href="#" style="color: white;">测试跳转页面链接</a>
+        </div>
+        <div style="margin:0 20px;display:inline;">
+            <router-link to="/" style="color: white;">Go to index</router-link>
+        </div>
+        <div style="margin:0 20px;display:inline;">
+            <router-link to="/list" style="color: white;">Go to list</router-link>
+        </div>
+        <div style="margin:0 20px;display:inline;">
+            <router-link to="/info" style="color: white;">Go to info</router-link>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: "test"
+    }
+</script>
